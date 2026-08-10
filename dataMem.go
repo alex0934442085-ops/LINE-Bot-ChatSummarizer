d440a24c3fc96e0faa404396e4b50370b50aae92
alex0934442085-ops <alex0934442085@gmail.com>
2026-08-10 13:22:04 +0800

    Update dataMem.go

--- dataMem.go
+++ dataMem.go
@@ -1,9 +1,12 @@
 package main
 
+import "time"
+
 type MemStorage map[string]GroupData
 
 type MemDB struct {
-	db MemStorage
+	db             MemStorage
+	summaryRecords map[string]time.Time
 }
 
 func (mdb *MemDB) ReadGroupInfo(roomID string) GroupData {
@@ -14,8 +17,17 @@ func (mdb *MemDB) AppendGroupInfo(roomID string, m MsgDetail) {
 	mdb.db[roomID] = append(mdb.db[roomID], m)
 }
 
+func (mdb *MemDB) GetLastSummaryTime(roomID string) time.Time {
+	return mdb.summaryRecords[roomID]
+}
+
+func (mdb *MemDB) SetLastSummaryTime(roomID string, t time.Time) {
+	mdb.summaryRecords[roomID] = t
+}
+
 func NewMemDB() *MemDB {
 	return &MemDB{
-		db: make(MemStorage),
+		db:             make(MemStorage),
+		summaryRecords: make(map[string]time.Time),
 	}
 }
