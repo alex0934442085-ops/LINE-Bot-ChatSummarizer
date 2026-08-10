fde0cd0b4bc50a35c6211dff757893a6ec209cb5
alex0934442085-ops <alex0934442085@gmail.com>
2026-08-10 13:21:51 +0800

    Update data.go

--- data.go
+++ data.go
@@ -5,7 +5,14 @@ import "time"
 type GroupDB interface {
 	ReadGroupInfo(string) GroupData
 	AppendGroupInfo(string, MsgDetail)
+
+	// 取得這個群組上一次統整的時間
+	GetLastSummaryTime(string) time.Time
+
+	// 記錄這個群組這次統整完成的時間
+	SetLastSummaryTime(string, time.Time)
 }
+
 type MsgDetail struct {
 	MsgText  string
 	UserName string
